dea37325abe352ebd01765483bef9b17c15d68ca
Ben Vincent <b.t.vincent@dundee.ac.uk>
2014-07-06 14:12:46 +0100

    initial commit

---
+++ demo_plot.m
@@ -0,0 +1,91 @@
+% demo_plot.m
+% Demonstrates use of the fill_between.m function
+
+
+
+figure(1), clf
+set(gcf,'Color',[1 1 1])
+%%
+% Example 1 ===============================================================
+subplot(2,2,1)
+x   = linspace(-pi,pi,200);
+y2  = sin(x*4);
+y1  = zeros(size(x));
+
+plot(x,y2,'k-')
+axis tight
+box off
+
+% define a binary vector of x values where we will fill
+where = x>0 & x<pi/2;
+
+% Define a set of patch properties that you want for your filled region
+% See http://www.mathworks.co.uk/help/matlab/ref/patch_props.html
+opts={'EdgeColor', 'none',...
+    'FaceColor',[0.5 0.5 1]};
+
+% Do the filling
+h = fill_between(x,y1,y2, where, opts);
+
+%%
+% Example 2 ===============================================================
+subplot(2,2,2)
+x   = linspace(-4,4,200);
+y2  = sin(x*4)/2;
+y1  = sin(x*2);
+
+plot(x,y1,'k-')
+hold on
+plot(x,y2,'k-')
+box off
+
+% Define a set of patch properties that you want for your filled region
+% See http://www.mathworks.co.uk/help/matlab/ref/patch_props.html
+opts={'EdgeColor', 'none',...
+    'FaceColor',[1 0.5 0.5]};
+
+% Do the filling
+fill_between(x,y1,y2, [], opts);
+
+%%
+% Example 3 ===============================================================
+subplot(2,2,3)
+x   = linspace(-4,4,200);
+y2  = sin(x*4)/2;
+y1  = sin(x*2);
+
+plot(x,y1,'k-')
+hold on
+plot(x,y2,'k-')
+box off
+
+% Do the filling
+opts={'EdgeColor', 'none',...
+    'FaceColor',[1 0.5 0.5]};
+fill_between(x,y1,y2, y1>y2, opts);
+
+opts={'EdgeColor', 'none',...
+    'FaceColor',[0.5 1 0.5]};
+fill_between(x,y1,y2, y1<y2, opts);
+
+
+%%
+% Example 4 ===============================================================
+subplot(2,2,4)
+x   = linspace(-4,4,600);
+y1  = ones(size(x))*0.5;
+y2  = sin(x*4);
+
+plot(x,y2,'k-')
+box off
+
+% Do the filling
+opts={'EdgeColor', 'none',...
+    'FaceColor',[1 0.5 0.5]};
+fill_between(x,y1,y2, y2>0.5, opts);
+
+% Do the filling
+opts={'EdgeColor', 'none',...
+    'FaceColor',[0.5 1 0.5]};
+fill_between(x,zeros(size(x)),y2, y2<0, opts);
+
---
+++ fill_between.m
@@ -0,0 +1,96 @@
+function h = fill_between(x,y1,y2, where, opts)
+% function originally written by Ben Vincent, July 2014. Inspired by a
+% function of the same name available in the Matplotlib Python library.
+
+% check hold status so we can return things to how they were 
+initialHoldState=ishold(gca);
+hold on
+
+% if no 'where' vector is provided...
+if numel(where)==0
+    where=ones(size(x));
+end
+
+%%
+% Check to see if 'where' contains just one zone, or many zones. We'll need
+% to draw a patch for each zone. We are going to create a vector which will
+% take on values of 0 for areas we will not fill, and areas >1 for areas we
+% will fill. This vector will be integer valued, the number describing the
+% region number.
+region=0;
+if where(1)==1
+    region=1;
+else
+    region=0;
+end
+cat(1) = region;
+% Now loop through the remaining entries
+for n=2:numel(where)
+    if where(n)==1 && where(n-1)==0
+        % new region
+        region=region+1;
+    end
+    if where(n)==0
+        cat(n)=0;
+    else
+        cat(n)=region;
+    end
+end
+
+% Now call the fill function for each  
+if max(cat)==0
+    error('no area to fill')
+end
+for n=1:max(cat)
+    % ---------------------------------
+    h(n) = fill_patch(x,y1,y2, cat==n);
+    % ---------------------------------
+end
+
+
+
+
+%% Apply formatting
+
+% cycle through options provided and apply them. These are patch properties
+% which are listed here:
+% http://www.mathworks.co.uk/help/matlab/ref/patch_props.html
+for n=1:2:numel(opts)
+    set(h, opts{n}, opts{n+1});
+end
+
+% move it to the back
+uistack(h,'bottom') 
+
+% return to initial hold state
+if initialHoldState==0
+	hold off
+end
+
+
+% make sure none of the fills run over the axes
+set(gca,'Layer','top')
+
+
+
+return
+    
+        
+
+
+
+
+function h=fill_patch(x,y1,y2, where)
+% Actually draw the filled patch 
+
+default_col=[1 0 0];
+
+x =[x(where==1),fliplr(x(where==1))];
+y =[y1(where==1),fliplr(y2(where==1))];
+
+% DRAW THE PATCH --------
+h=patch(x,y,default_col);
+% -----------------------
+return
+
+return
